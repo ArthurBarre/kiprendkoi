e8633263bf9eafb7a673e0adb39ed11a5951ec04
Arthur Barré <arthur.barre@easy.movie>
2020-03-17 13:27:58 +0100

    post party && post item good now

--- app.js
+++ app.js
@@ -2,11 +2,42 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 const dotenv = require("dotenv").config();
+const bodyParser = require("body-parser");
+const axios = require("axios");
 
 app.set("view engine", "pug");
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/", function(req, res) {
   res.render("index");
+});
+
+app.post("/party", (req, res) => {
+  axios
+    .post(`${process.env.API_URL}/party`, req.body)
+    .then(({ data }) => res.redirect(`/party/${data._id}`))
+    .catch(err => res.send(err));
+});
+
+app.post("/party/:id/items", (req, res) => {
+  axios
+    .post(`${process.env.API_URL}/party/${req.params.id}/items`, req.body)
+    .then(() => res.redirect(`/party/${req.params.id}`))
+    .catch(err => res.send(err));
+});
+
+app.get("/party/:id", (req, res) => {
+  axios
+    .get(`${process.env.API_URL}/party/${req.params.id}`)
+    .then(({ data }) =>
+      res.render("party", {
+        party: data,
+        title: data.name,
+        url: `${process.env.FRONT_URL}:${process.env.PORT}/party/${data._id}`
+      })
+    )
+    .catch(err => console.log(err));
 });
 
 app.listen(port, () => console.log(`Front app listening on port ${port}!`));
