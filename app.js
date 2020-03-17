0756ad9627a2332be13db4b2c48a91d69171cbd3
Arthur Barré <arthur.barre@easy.movie>
2020-03-17 10:51:48 +0100

    init app and install all libs

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+const dotenv = require("dotenv").config();
+
+app.set("view engine", "pug");
+
+app.get("/", function(req, res) {
+  res.render("index");
+});
+
+app.listen(port, () => console.log(`Front app listening on port ${port}!`));
