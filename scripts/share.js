a141c49002dfaa237453b6adf4262a2398adf689
Arthur Barré <arthur.barre@easy.movie>
2020-03-21 12:13:36 +0100

    add share features

--- scripts/share.js
+++ scripts/share.js
@@ -1,16 +1,21 @@
 exports.share = function() {
-  const $shareEl = document.getElementById("url");
+  if (navigator.share) {
+    console.log("supported");
+  } else console.log("not");
   const shareButton = document.getElementById("share");
-  shareButton.addEventListener("click", function() {
-    navigator
-      .share({
-        title: $shareEl.getAttribute("data-share-title"),
-        text: $shareEl.getAttribute("data-share-text"),
-        url: $shareEl.getAttribute("data-share-url")
-      })
-      .then(
-        () => console.log("Successful share"),
-        error => console.log("Error sharing:", error)
-      );
-  });
+  const $shareEl = document.getElementById("url");
+  if (shareButton) {
+    shareButton.addEventListener("click", function() {
+      navigator
+        .share({
+          title: $shareEl.getAttribute("data-share-title"),
+          text: $shareEl.getAttribute("data-share-text"),
+          url: $shareEl.getAttribute("data-share-url")
+        })
+        .then(
+          () => console.log("Successful share"),
+          error => console.log("Error sharing:", error)
+        );
+    });
+  }
 };
